6f692c1e840e1c737b26629abbe881e8b61348f4
DevinAugot <105358840+DevinAugot@users.noreply.github.com>
2023-02-16 12:43:15 -0330

    Project initialized

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const restAPP = express();
+const pgDBAccessLayer = require("./pg_data_access/pg_db_access");
+const port = 3000;
+
+restAPP.use(bodyParser.json());
+restAPP.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+restAPP.get("/", (request, response) => {
+  response.json({ info: "Node.js, Express, and Postgres API" });
+});
+
+restAPP.get("/students", pgDBAccessLayer.getStudents);
+restAPP.post("/student", pgDBAccessLayer.createStudent);
+restAPP.put("/student/:id", pgDBAccessLayer.updateStudent);
+restAPP.delete("/student/:id", pgDBAccessLayer.deleteStudent);
+
+// start the Express server
+restAPP.listen(port, () => {
+  console.log(`App running on port ${port}.`);
+});
